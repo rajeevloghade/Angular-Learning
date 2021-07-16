2a888bc73648d281af8d293f2df71120caec8b7d
rajeevloghade <rajeevloghade010@gmail.com>
2021-07-16 16:38:44 +0530

    property binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,17 +4,17 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-test', //as a class name
   // selector: '[app-test]', //as a attribute name
   selector: 'app-test',
-  // templateUrl: './test.component.html',
-  template: `<div>
-    <h2 style=color:black>Interpolation concept</h2>
-    <h2>Welcome {{ name }}</h2>
-    <h2>{{ 2 + 2 }}</h2>
-    <h2>{{ 'Welcome ' + name }}</h2>
-    <h2>{{ name.length }}</h2>
-    <h2>{{ name.toUpperCase() }}</h2>
-    <h2>{{ greetUser() }}</h2>
-    <h2>{{ siteUrl }}</h2>
-  </div>`,
+  templateUrl: './test.component.html',
+//   template: `<div>
+//   <h2 style=color:black>Interpolation concept</h2>
+//   <h2>Welcome {{ name }}</h2>
+//   <h2>{{ 2 + 2 }}</h2>
+//   <h2>{{ 'Welcome ' + name }}</h2>
+//   <h2>{{ name.length }}</h2>
+//   <h2>{{ name.toUpperCase() }}</h2>
+//   <h2>{{ greetUser() }}</h2>
+//   <h2>{{ siteUrl }}</h2>
+// </div>`,
   // styleUrls: ['./test.component.css']
   styles: [
     `
@@ -27,6 +27,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   public name = 'Rajeev Loghade';
   public siteUrl = window.location.href;
+  public myId="testId";
   constructor() {}
 
   ngOnInit(): void {}
