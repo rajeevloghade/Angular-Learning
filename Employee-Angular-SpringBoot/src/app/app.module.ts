5dd990c577e98d2810b35a3ba7d3979313ed43ce
rajeevloghade <rajeevloghade010@gmail.com>
2021-07-20 11:19:19 +0530

    Components and services created

--- Employee-Angular-SpringBoot/src/app/app.module.ts
+++ Employee-Angular-SpringBoot/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
