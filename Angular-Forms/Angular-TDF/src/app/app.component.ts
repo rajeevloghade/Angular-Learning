82eb2205829cf540523461d1476171de31eab284
rajeevloghade <rajeevloghade010@gmail.com>
2021-07-24 12:36:23 +0530

    Applied two way binding

--- Angular-Forms/Angular-TDF/src/app/app.component.ts
+++ Angular-Forms/Angular-TDF/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  topics=['Angular','React','Vue']
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Rob', 'rob@gmail.com', 7638475643, '', 'morning', true);
 }
