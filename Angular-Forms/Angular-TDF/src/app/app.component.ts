0b4b0575a0fb4d6579aa65f0fdae3ff5d591d0bc
rajeevloghade <rajeevloghade010@gmail.com>
2021-07-24 14:07:17 +0530

    validations and errors

--- Angular-Forms/Angular-TDF/src/app/app.component.ts
+++ Angular-Forms/Angular-TDF/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { User } from './user';
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
 
-  userModel = new User('Rob', 'rob@gmail.com', 7638475643, '', 'morning', true);
+  userModel = new User('', 'rob@gmail.com', 7638475643, '', 'morning', true);
 }
